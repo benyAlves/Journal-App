12e80b01d5e12ba5bbd5cf0f55fcf888cb0e92fb
bernardo <dalves@dabula.co.mz>
2018-06-28 01:54:15 +0200

    Application class

---
+++ app/src/main/java/challenge/code/andela/udacity/journalapp/JournalApp.java
@@ -0,0 +1,16 @@
+package challenge.code.andela.udacity.journalapp;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class JournalApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+
+
+}
