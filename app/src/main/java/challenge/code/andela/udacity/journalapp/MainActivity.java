18072f664d5b19194aeafe595344a5cc2a28a3b9
bernardo <dalves@dabula.co.mz>
2018-06-28 00:46:47 +0200

    updating mainActivity

--- app/src/main/java/challenge/code/andela/udacity/journalapp/MainActivity.java
+++ app/src/main/java/challenge/code/andela/udacity/journalapp/MainActivity.java
@@ -2,44 +2,86 @@ package challenge.code.andela.udacity.journalapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import challenge.code.andela.udacity.journalapp.model.JournalEntry;
+import challenge.code.andela.udacity.journalapp.utils.Utils;
+
+public class MainActivity extends AppCompatActivity implements OnClickJournalEntry{
 
     private static final int RC_SIGN_IN = 111;
     private static final String TAG = "MainActivity";
+    public static final String EXTRA_JOURNAL_ITEM = "journal-item";
+    public static final String EXTRA_JOURNAL_IMAGE_TRANSITION_NAME = "transition-name";
+    private static final String DATA = "data";
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    @BindView(R.id.my_recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
+    private ChildEventListener mChildEventListener;
+    private EntryAdapter entryAdapter;
+    private ArrayList<JournalEntry> listEntries;
+    private DatabaseReference mDatabase;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child(Utils.FIREBASE_CHILD).child(mFirebaseAuth.getUid());
+        listEntries = new ArrayList<>();
+
+        showProgressBar();
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (mFirebaseAuth.getCurrentUser() != null) {
-                    // already signed in
-                    Toast.makeText(MainActivity.this, "Already Signed in", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "onCreate");
+                    if(savedInstanceState == null) {
+                        attachDatabaseReadListener();
+                    }else {
+                        hideProgressBar();
+                        listEntries = savedInstanceState.getParcelableArrayList(DATA);
+                        entryAdapter = new EntryAdapter(listEntries, getApplicationContext(), MainActivity.this);
+                        recyclerView.setAdapter(entryAdapter);
+                    }
                 } else {
-                    Toast.makeText(MainActivity.this, "Not Signed in", Toast.LENGTH_SHORT).show();
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
@@ -55,6 +97,20 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(linearLayoutManager);
+        entryAdapter = new EntryAdapter(listEntries, getApplicationContext(), this);
+        recyclerView.setAdapter(entryAdapter);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putParcelableArrayList(DATA, listEntries);
     }
 
     @Override
@@ -65,12 +121,10 @@ public class MainActivity extends AppCompatActivity {
 
             // Successfully signed in
             if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
             } else {
                 // Sign in failed
                 if (response == null) {
                     // User pressed back button
-                    showToast(R.string.sign_in_cancelled);
                     finish();
                 }
 
@@ -112,6 +166,11 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.sign_out_menu:
                 AuthUI.getInstance().signOut(this);
+                detachDatabaseReadListener();
+                return true;
+            case R.id.add_entry:
+                Intent intent = new Intent(this, WriteJournalActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -123,4 +182,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(int adapterPosition, JournalEntry journalEntry) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_JOURNAL_ITEM, journalEntry);
+        //intent.putExtra(EXTRA_JOURNAL_IMAGE_TRANSITION_NAME, ViewCompat.getTransitionName(imageViewEntry));
+
+//        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+//                this,
+//                imageViewEntry,
+//                ViewCompat.getTransitionName(imageViewEntry));
+
+        startActivity(intent);
+    }
+
+    private void attachDatabaseReadListener() {
+            if (mChildEventListener == null) {
+                mChildEventListener = new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        hideProgressBar();
+                        JournalEntry journalEntry = dataSnapshot.getValue(JournalEntry.class);
+                        listEntries.add(journalEntry);
+                        entryAdapter.notifyDataSetChanged();
+                    }
+
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        entryAdapter.notifyDataSetChanged();
+                    }
+
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    }
+
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                };
+                mDatabase.addChildEventListener(mChildEventListener);
+            }
+    }
+
+    private void detachDatabaseReadListener() {
+        if (mChildEventListener != null) {
+            mDatabase.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
+    }
+
+    public void showProgressBar(){
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void hideProgressBar(){
+        progressBar.setVisibility(View.GONE);
+    }
 }
