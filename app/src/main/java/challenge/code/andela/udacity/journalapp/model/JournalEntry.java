1c35a715b8fb29e16cbf4004bc7877603c4a5882
bernardo <dalves@dabula.co.mz>
2018-06-28 00:53:26 +0200

    model updates

--- app/src/main/java/challenge/code/andela/udacity/journalapp/model/JournalEntry.java
+++ app/src/main/java/challenge/code/andela/udacity/journalapp/model/JournalEntry.java
@@ -1,11 +1,19 @@
 package challenge.code.andela.udacity.journalapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class JournalEntry {
+@IgnoreExtraProperties
+public class JournalEntry implements Parcelable{
 
+    private String pushId;
+    private String title;
     private String body;
     private String imageUrl;
     private String entryDate;
@@ -16,8 +24,49 @@ public class JournalEntry {
         this.entryDate = entryDate;
     }
 
+    public JournalEntry(String title, String body, String imageUrl, String entryDate) {
+        this.title = title;
+        this.body = body;
+        this.imageUrl = imageUrl;
+        this.entryDate = entryDate;
+    }
+
     public JournalEntry() {
     }
+
+    protected JournalEntry(Parcel in) {
+        body = in.readString();
+        imageUrl = in.readString();
+        entryDate = in.readString();
+        title = in.readString();
+        pushId = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(body);
+        dest.writeString(imageUrl);
+        dest.writeString(entryDate);
+        dest.writeString(title);
+        dest.writeString(pushId);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<JournalEntry> CREATOR = new Creator<JournalEntry>() {
+        @Override
+        public JournalEntry createFromParcel(Parcel in) {
+            return new JournalEntry(in);
+        }
+
+        @Override
+        public JournalEntry[] newArray(int size) {
+            return new JournalEntry[size];
+        }
+    };
 
     public String getBody() {
         return body;
@@ -44,7 +93,19 @@ public class JournalEntry {
     }
 
 
-    public String dateToString(Date date) {
-        return new SimpleDateFormat("hh:mm").format(date);
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
     }
 }
